1cadc6c8aab253204e63dadfce8452e035b15f95
mugishakundasarah <mugishakundasarah@gmail.com>
2022-03-03 06:02:27 +0200

    Learnt enhancers and adding middleware to the stock

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
-import { createStore } from "redux";
+import { applyMiddleware , createStore } from "redux";
 import rootReducer from "./reducer";
+import {print1, print2, print3} from './exampleAddons/middleware'
+import { composeWithDevTools } from "redux-devtools-extension";
+
 
 let preloadedState
 const persistedTodosString = localStorage.getItem('todos')
@@ -10,6 +13,11 @@ if(persistedTodosString){
     }
 }
 
-const store = createStore(rootReducer, preloadedState)
+const composedEnhancer = composeWithDevTools(
+    applyMiddleware(print1, print2, print3)
+)
+
+// Pass enhancer as teh second arg, since there's no preloaded state
+const store = createStore(rootReducer, composedEnhancer)
 
 export default store
